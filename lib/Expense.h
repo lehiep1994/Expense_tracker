30703c94648a311a1fef81c15d97170d5f1eed11
lehiep1994 <lephuochiep.1994.1911@gmail.com>
2025-12-28 04:14:16 +0000

    The first stage in making changes for my own software.

---
+++ lib/Expense.h
@@ -0,0 +1,9 @@
+#ifdef EXPENSE_H
+#define EXPENSE_H
+
+typedef struct {
+    char description[100];
+    float amount;
+} Expense;
+
+#endif // EXPENSE_H
---
+++ src/main.c
@@ -0,0 +1,7 @@
+// This is where i start to build my own expense tracker in C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "../lib/Expense.h"
